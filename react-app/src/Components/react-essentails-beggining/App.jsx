a8224ab9927e5665528ec92e671dcc36cb17dcbd
emir-marukic <marukicemir33@hotmail.com>
2024-01-22 18:53:43 +0100

    .

---
+++ react-app/src/Components/react-essentails-beggining/App.jsx
@@ -0,0 +1,64 @@
+import img from "./assets/react-core-concepts.png";
+import componentImg from "./assets/components.png";
+import { CORE_CONCEPTS } from "./data";
+
+const reactDescription = ["Core", "Fundamental", "Crucial"];
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+export function Header() {
+  const description = reactDescription[getRandomInt(2)];
+  return (
+    <>
+      <header>
+        <img src={img} alt="Stylized atom" />
+        <h1>React Essentials</h1>
+        <p>
+          {description} React concepts you will need for almost any app you are
+          going to build!
+        </p>
+      </header>
+    </>
+  );
+}
+
+export function CoreConcepts(props) {
+  return (
+    <div>
+      <li>
+        <img src={props.img} alt="#" />
+        <h3>{props.title}</h3>
+        <p>{props.text}</p>
+      </li>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Core concept</h2>
+          <ul>
+            {CORE_CONCEPTS.map((item, index) => {
+              return (
+                <CoreConcepts
+                  key={index}
+                  title={item.title}
+                  text={item.description}
+                  img={item.image}
+                />
+              );
+            })}
+          </ul>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
--- react-app/src/main.jsx
+++ react-app/src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./Testt/app";
+import App from "./Components/react-essentails-beggining/App";
+import "./Components/react-essentails-beggining/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
