55eb4daf53ce619ebf028e3145ea8b997d6abd04
emir-marukic <marukicemir33@hotmail.com>
2024-02-27 16:48:17 +0100

    Context

--- 04-api-context/src/components/Cart.jsx
+++ 04-api-context/src/components/Cart.jsx
@@ -1,8 +1,8 @@
 import { useContext } from "react";
 import { CartContext } from "../store/shoppingCartContext";
 
-export default function Cart({ items, onUpdateItemQuantity }) {
-  useContext(CartContext);
+export default function Cart({ onUpdateItemQuantity }) {
+  const { items } = useContext(CartContext);
   const totalPrice = items.reduce(
     (acc, item) => acc + item.price * item.quantity,
     0
